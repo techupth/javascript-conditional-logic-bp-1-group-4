7d64d0f1d03ababda6624c86600111fdc550fbf8
DESKTOP-K9VCJKV\163Shinna <chinsihabut@gmail.com>
2023-10-01 21:50:01 +0700

    Assignment: Conditional Logic

--- ex-2.js
+++ ex-2.js
@@ -2,3 +2,15 @@
 let lightBulbStatus = "On";
 
 // Start coding here.
+lightBulbStatus = "Broken";
+
+if (lightBulbStatus === "On"){
+    console.log("Light bulb is On")
+}else if (lightBulbStatus === "Off"){
+    console.log("Lifht bulb is Off")
+}else if (lightBulbStatus === "Broken"){
+    console.log("Light bulb is Broken")
+}else{
+    console.log("PLease choose the correct input")
+}
+
